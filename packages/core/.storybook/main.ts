3316417d3a5034deadd69323af038be9b64632e9
Joe <orenbek.serk@gmail.com>
2024-03-18 14:29:55 +0800

    chore: add storybook

---
+++ packages/core/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@modern-js/storybook';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@modern-js/storybook',
+    options: {
+      bundler: 'rspack',
+    },
+  },
+  typescript: {
+    reactDocgen: 'react-docgen'
+  }
+};
+
+export default config;
